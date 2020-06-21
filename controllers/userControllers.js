14f3e50e12e382ee553b9be4e294f0f11c5e28d8
Prabhakar Acharya <acharyaP03@gmail.com>
2020-06-21 22:31:26 +1000

    Logging in user with comparing password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,21 @@
-exports.getAllUsers = (req, res)=>{
-    res.status(500).json({
-        status : 'Error',
-        message : 'This Route is not defined yet....'
-    })
-}
+const User = require('./../models/userModel')
+const APIFeatures = require('./../utils/apiFeatures');
+const AppError = require('./../utils/appError')
+const catchAsync = require('./../utils/catchAsync');
+
+
+exports.getAllUsers = catchAsync( async (req, res)=>{
+    const users = await User.find()
+     //send the response..
+     res.status(200).json({
+         status : 'success',
+         requestedAt : req.requestTime,
+         results : users.length,
+         data : {
+             users
+         }
+     })
+});
 
 exports.getUser = (req, res)=>{
     res.status(500).json({
