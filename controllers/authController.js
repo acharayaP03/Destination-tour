bd9581233a4dfa2e7a38e0b0975fc865647cd06b
Prabhakar Acharya <acharyaP03@gmail.com>
2020-06-23 20:06:30 +1000

    sending jwt pasword reset roken via email

--- controllers/authController.js
+++ controllers/authController.js
@@ -132,29 +132,34 @@ exports.forgotPassword = catchAsync(async (req, res, next) =>{
     await user.save({ validateBeforeSave: false})
 
     // 3) Send it to user's email
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-    const message = `Forgot your pasowrd? submit a new pasword request with new pasword and confirm password to ${resetUrl}.\n If you didnot request this, then simply ignore this email.`
+    const resetURL = `${req.protocol}://${req.get(
+        'host'
+      )}/api/v1/users/resetPassword/${resetToken}`;    
+      const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
     
 
     // if email sent i failed the catch block is envoked.
     try{
         await sendEmail({
             email: user.email,
-            subject: 'Your password reset token (Only vaild for 30 mins)',
+            subject: 'Your password reset token (valid for 10 min)',
             message
-        });
+          });
     
-        res.status(200).json({
+          res.status(200).json({
             status: 'success',
-            message: 'Token sent to email'
-        })
+            message: 'Token sent to email!'
+          });
     }catch(err){
         user.passwordResetToken = undefined;
         user.passwordResetExpires = undefined;
 
         await user.save({ validateBeforeSave: false})
 
-        return next( new AppError(' There was an error sending email, please try again later.', 500))
+        return next(
+            new AppError('There was an error sending the email. Try again later!'),
+            500
+          );
     }
 
 })
--- utils/email.js
+++ utils/email.js
@@ -1,32 +1,59 @@
+// const nodemailer = require('nodemailer');
+
+// const sendEmail = async (options) =>{
+
+//     // 1. Create a Transporter.
+//     const transporter = nodemailer.createTransport({
+//         //if we want to use any sevices like gmail or outlook. Not recommended in devlopment mode. we could get marke as a spammer. 
+//          //TO USE GAMIL ACTIVATE " less secure app" options.
+//         //service: 'Gmail',
+//         host: process.env.EMAIL_HOST,
+//         port: process.env.EMAIL_PORT,
+//         auth: {
+//             user: process.env.EMAIL_USERNAME,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+       
+//     })
+//     // 2. Define the email options
+//     const mailOptions = {
+//         from: 'tour Nature <admin@email.com>',
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message
+//     }
+
+//     // 3. Send the Email.
+//     await transporter.sendMail(mailOptions)
+
+// }
+
+// module.exports = sendEmail;
+
 const nodemailer = require('nodemailer');
 
-const sendEmail = async options =>{
-
-    // 1. Create a Transporter.
-    const transporter = nodemailer.createTransport({
-        //if we want to use any sevices like gmail or outlook. Not recommended in devlopment mode. we could get marke as a spammer. 
-         //TO USE GAMIL ACTIVATE " less secure app" options.
-        //service: 'Gmail',
+const sendEmail = async options => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
         }
-       
-    })
-    // 2. Define the email options
-    const mailOptions = {
-        from: 'tour Nature <admin@email.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    }
+    });
 
-    // 3. Send the Email.
-    await transporter.sendMail(mailOptions)
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+    // html:
+  };
 
-}
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
 
 module.exports = sendEmail;
-
